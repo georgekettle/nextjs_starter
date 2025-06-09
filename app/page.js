a8df251ad14c4ced84bf07dbbdf18e53ce1ccc58
george kettle <georgekettle.gk@gmail.com>
2025-06-09 14:51:08 +1000

    Changed port to 3001

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
       <main className="flex flex-col gap-6 row-start-2 items-center sm:items-start max-w-2xl">
         <div className="flex flex-col gap-2 text-center sm:text-left">
           <h1 className="text-4xl tracking-tighter text-balance max-lg:font-medium sm:text-5xl lg:text-6xl xl:text-8xl">Ruby on Rails backend.<br/>NextJS frontend.</h1>
